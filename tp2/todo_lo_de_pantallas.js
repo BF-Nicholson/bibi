317e78293fa4e667070d883dfb36e5666698c5ee
BF-Nicholson <168227057+BF-Nicholson@users.noreply.github.com>
2024-11-12 16:58:08 -0300

    Add files via upload
    avance de tp4 (cambiar el nombre del archivo despues)

---
+++ tp2/todo_lo_de_pantallas.js
@@ -0,0 +1,11 @@
+function pantalladeinicio(){
+  textSize(20);
+  if (estado ===1){
+    fill(0, 50, 200);
+    rect(0, 0, widthh, heightt);
+    image(theboys, 50, 400, 100, 100);
+    image(titulo, 320, 150, 200, 200);
+    
+  }
+  
+}
