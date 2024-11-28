3e797836ec2c02dabecfb37cf5fcd0b567ff6514
BF-Nicholson <168227057+BF-Nicholson@users.noreply.github.com>
2024-11-28 17:39:00 -0300

    Delete tpfinalparte2 directory

--- tpfinalparte2/Mordecai.js
+++
@@ -1,9 +0,0 @@
-class mordecai {
-  constructor() {
-    this.posX = 50-10;
-    this.posY = 50-10;
-  }
-  dibujar() {
-    this.sprite = image(mordecaisprite, this.posX, this.posY);
-  }
-}
--- tpfinalparte2/Rigby.js
+++
@@ -1,9 +0,0 @@
-class rigby {
-  constructor(posX, posY) {
-    this.posX = posX;
-    this.posY = posY;
-  }
-  dibujar() {
-    this.sprite = image(rigbysprite, this.posX, this.posY);
-  }
-}
