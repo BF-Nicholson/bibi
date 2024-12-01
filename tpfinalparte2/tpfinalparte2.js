1621f623c9ba1416c310dcbd1895cb08f322497e
BF-Nicholson <168227057+BF-Nicholson@users.noreply.github.com>
2024-11-30 21:50:56 -0300

    tpfinalparte2

---
+++ tpfinalparte2/tpfinalparte2.js
@@ -0,0 +1,43 @@
+let rigbysprite;
+let mordecaisprite;
+let bensonsprite;
+let objJuego;
+let miCancion;
+let SoundTrackSuena = false;
+
+function preload() {
+  soundFormats('mp3');
+  miCancion = loadSound('data/cancion1.mp3');
+  
+}
+function setup() {
+  createCanvas(640, 480);
+  background(170);
+
+  rigbysprite = loadImage('data/rigbysprite.png');
+  mordecaisprite = loadImage('data/mordecaisprite.png');
+  bensonsprite = loadImage('data/bensonsprite.png');
+
+  objJuego = new Juego(8);
+  objJuego.Loop(miCancion);
+  
+  
+}
+
+
+function draw() {
+  background(150);
+  objJuego.dibujar();
+  objJuego.Perseguir();
+  objJuego.Fiesta();
+objJuego.Ganar();
+  //print("mouseX:"+mouseX);
+  //print("mouseY:"+mouseY);
+  print("keycODE:"+key);
+}
+
+function keyPressed() {
+  objJuego.teclaPresionada(key);
+  objJuego.Musica(key, miCancion);
+  
+}
